0b7f0e8af36080635064fcab392794f04a09d291
Reedik Poopuu <reedik.poopuu@gmail.com>
2025-04-19 16:07:43 +0300

    Added Relational Operators class

---
+++ src/RelationalOperators.java
@@ -0,0 +1,24 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Equal to (==)
+        System.out.println("a == b: " + (a == b)); // false
+
+        // Not equal to (!=)
+        System.out.println("a != b: " + (a != b)); // true
+
+        // Greater than (>)
+        System.out.println("a > b: " + (a > b)); // false
+
+        // Less than (<)
+        System.out.println("a < b: " + (a < b)); // true
+
+        // Greater than or equal to (>=)
+        System.out.println("a >= b: " + (a >= b)); // false
+
+        // Less than or equal to (<=)
+        System.out.println("a <= b: " + (a <= b)); // true
+    }
+}
